faa0a16392f213ae759d571b610fba498ee37e53
Arnau Mora <arnyminerz@proton.me>
2023-08-21 14:54:35 +0200

    Fixed image clipping
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- app/src/main/java/org/escalaralcoiaicomtat/android/activity/SectorViewer.kt
+++ app/src/main/java/org/escalaralcoiaicomtat/android/activity/SectorViewer.kt
@@ -40,6 +40,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -198,6 +199,7 @@ class SectorViewer : AppCompatActivity() {
                 modifier = Modifier
                     .fillMaxHeight()
                     .weight(1f)
+                    .clipToBounds()
             ) {
                 AsyncImage(
                     model = ImageRequest.Builder(context)
