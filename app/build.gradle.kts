ea4825f5cd57c215e0df2025b3fb0f7c4c47f9c2
Arnau Mora <arnyminerz@proton.me>
2023-08-23 13:19:59 +0200

    Using alpha activity dependency
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.0"
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
 
     packaging {
@@ -67,10 +67,11 @@ val workVersion by project.properties
 val ktorVersion by project.properties
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-ktx:1.8.0-alpha06")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
 
     // Jetpack Compose
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
