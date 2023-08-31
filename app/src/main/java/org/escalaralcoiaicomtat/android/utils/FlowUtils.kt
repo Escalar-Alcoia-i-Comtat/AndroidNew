b2a73a43b559044a8ca6782aa87f37b29e0c7781
Arnau Mora <arnyminerz@proton.me>
2023-08-31 15:43:26 +0200

    Made functions inline
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- app/src/main/java/org/escalaralcoiaicomtat/android/utils/FlowUtils.kt
+++ app/src/main/java/org/escalaralcoiaicomtat/android/utils/FlowUtils.kt
@@ -1,13 +1,13 @@
 package org.escalaralcoiaicomtat.android.utils
 
-fun <T> T.letIf(condition: Boolean, block: (T) -> T) =
+inline fun <T> T.letIf(condition: Boolean, block: (T) -> T) =
     if (condition) {
         block(this)
     } else {
         this
     }
 
-fun <T, O> T.letIfNotNull(obj: O?, block: T.(O) -> T) =
+inline fun <T, O> T.letIfNotNull(obj: O?, block: T.(O) -> T) =
     if (obj != null) {
         block(this, obj)
     } else {
