f6decc0ac48da2cceaef9750c79dc33a725ddc7a
Arnau Mora <arnyminerz@proton.me>
2023-08-31 16:21:31 +0200

    Added display name
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- app/src/main/java/org/escalaralcoiaicomtat/android/storage/type/EndingInclination.kt
+++ app/src/main/java/org/escalaralcoiaicomtat/android/storage/type/EndingInclination.kt
@@ -1,5 +1,10 @@
 package org.escalaralcoiaicomtat.android.storage.type
 
-enum class EndingInclination {
-    VERTICAL, DIAGONAL, HORIZONTAL
+import androidx.annotation.StringRes
+import org.escalaralcoiaicomtat.android.R
+
+enum class EndingInclination(@StringRes val displayName: Int) {
+    VERTICAL(R.string.ending_inclination_vertical),
+    DIAGONAL(R.string.ending_inclination_diagonal),
+    HORIZONTAL(R.string.ending_inclination_horizontal)
 }
--- app/src/main/java/org/escalaralcoiaicomtat/android/storage/type/EndingInfo.kt
+++ app/src/main/java/org/escalaralcoiaicomtat/android/storage/type/EndingInfo.kt
@@ -1,5 +1,10 @@
 package org.escalaralcoiaicomtat.android.storage.type
 
-enum class EndingInfo {
-    RAPPEL, EQUIPPED, CLEAN
+import androidx.annotation.StringRes
+import org.escalaralcoiaicomtat.android.R
+
+enum class EndingInfo(@StringRes val displayName: Int) {
+    RAPPEL(R.string.ending_info_rappel),
+    EQUIPPED(R.string.ending_info_equipped),
+    CLEAN(R.string.ending_info_clean)
 }
