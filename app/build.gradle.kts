e3b2e52775c2462612d0e0b3566a052d249e8eb8
Arnau Mora <arnyminerz@proton.me>
2023-09-09 12:34:50 +0200

    Updated dependencies
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -139,7 +139,8 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
 
     // Jetpack Compose
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
@@ -150,9 +151,9 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-    implementation("androidx.navigation:navigation-compose:2.7.1")
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.navigation:navigation-compose:2.7.2")
 
     // Jetpack Compose - Reorderable lists
     implementation("org.burnoutcrew.composereorderable:reorderable:0.9.6")
