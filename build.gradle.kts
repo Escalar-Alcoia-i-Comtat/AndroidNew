89440667370b184d31b64480b6af44d70eb25e13
Arnau Mora <arnyminerz@proton.me>
2023-12-24 19:21:37 +0100

    Migrated to Gradle version catalog
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all subprojects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
-    id("com.google.devtools.ksp") version "1.9.21-1.0.16" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.21" apply false
-    id("app.brant.amazonappstorepublisher") version "0.1.0" apply false
+    alias(libs.plugins.amazonappstorepublisher) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ksp) apply false
 }
